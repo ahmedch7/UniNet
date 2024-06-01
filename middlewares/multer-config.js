a7c4c6112c5a9e5722367f9b5985362fe7f3f969
ahmedcheikhrouhou <ahmed.cheikhrouhou80@gmail.com>
2024-06-01 11:53:35 +0100

    multer config added

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import multer, { diskStorage } from "multer";
-import { join, dirname, extname } from "path";
-import { fileURLToPath } from "url";
-
-const MIME_TYPES = {
-    "image/jpg": "jpg",
-    "image/jpeg": "jpg",
-    "image/png": "png",
-};
-
-export default function (image) {
-    return multer({
-        storage: diskStorage({
-            destination: (req, file, callback) => {
-                //C:\Users\yassi\Desktop\node\4sim1\workshop-gse-gamix2122\middlewares
-                //C:\Users\yassi\Desktop\node\4sim4\exercice_s4\models\game.js
-                const __dirname = dirname(fileURLToPath(import.meta.url));
-                callback(null, join(__dirname, "../public/images"));
-            },
-            filename: (req, file, callback) => {
-                const name = file.originalname.split(" ").join("_");
-                const extension = MIME_TYPES[file.mimetype];
-                let newFileName = +new Date() + extname(file.originalname);
-                callback(null, newFileName);
-            },
-        }),
-        limits: 5 * 1024 * 1024,
-    }).single(image);
-}
-=======
 import multer, { diskStorage } from "multer";
 import { join, dirname, extname } from "path";
 import { fileURLToPath } from "url";
@@ -56,4 +25,3 @@ export default function (image, size) {
     limits: size,
   }).single(image);
 }
->>>>>>> origin/TalelKbaier
