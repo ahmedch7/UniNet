bbbf2a0945579259547c4bbaf8170dc185520d35
ahmedcheikhrouhou <ahmed.cheikhrouhou80@gmail.com>
2024-06-01 11:53:09 +0100

    stripe payment added

--- server.js
+++ server.js
@@ -9,20 +9,18 @@ import 'dotenv/config'
 import niveauRouter from "./routes/niveauEtude.route.js";
 import coursRouter from "./routes/cours.route.js";
 import classeRouter from "./routes/classe.route.js";
-import studentRouter from "./routes/student.route.js"
-import userRoutes from './routes/userRoutes.js';
 import foyerRoutes from './routes/foyerRoutes.js';
 import menuRoutes from './routes/menuRoutes.js';
 import reservationRoutes from './routes/reservationRoutes.js';
 import commentRoutes from './routes/commentRoutes.js'; 
 import restaurantRoutes from './routes/restaurantRoutes.js';  
-import roomRoutes from './routes/roomRoutes.js';  
+import roomRoutes from './routes/roomRoutes.js';
+import paymentRoutes from './routes/payment.route.js';
 import { errorHandler, notFoundError } from "./middlewares/error-handler.js";
 
 
 
 const app = express();
-const port = process.env.PORT || 9090;
 const databaseName = "uninet";
 
 mongoose.connect(`mongodb://localhost:27017/${databaseName}`, { family: 4 })
@@ -32,7 +30,6 @@ mongoose.connect(`mongodb://localhost:27017/${databaseName}`, { family: 4 })
     .catch((e) => {
         console.log(e);
     });
-//test
 
 app.use(cors());
 app.use(express.json());
@@ -40,8 +37,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
- 
-app.use('/api/users', userRoutes);
+const PORT = process.env.PORT || 9090;
+const hostname = '127.0.0.1';
+
 app.use('/api/foyers', foyerRoutes);
 app.use('/api/menus', menuRoutes);
 app.use('/api/reservations', reservationRoutes); 
@@ -53,15 +51,15 @@ app.use('/api/rooms', roomRoutes);
 app.use('/api/auth', authRoutes)
 app.use("/user", userRouter)
 app.use("/university", universityRouter)
+app.use('/api/payment', paymentRoutes);
 
 app.use("/niveauEtude", niveauRouter );
 app.use("/cours", coursRouter );
 app.use("/classe", classeRouter );
-app.use("/student", studentRouter );
 
 
 app.use(notFoundError)
 app.use(errorHandler)
 app.listen(PORT, hostname, () => {
-    console.log(`server running on http://${hostname}:${PORT}`);
-})
+    console.log(`Server running on http://${hostname}:${PORT}`);
+  });
