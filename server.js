c05cb507354a083abbd8c4c20a230ee386bef5cd
ahmedcheikhrouhou <ahmed.cheikhrouhou80@gmail.com>
2024-06-01 12:51:45 +0100

    merge azizbranch into main

--- server.js
+++ server.js
@@ -4,33 +4,37 @@ import cors from "cors";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import universityRouter from "./routes/university.route.js";
-import authRoutes from "./routes/auth.route.js"
-import 'dotenv/config'
+import authRoutes from "./routes/auth.route.js";
+import "dotenv/config";
 import niveauRouter from "./routes/niveauEtude.route.js";
 import coursRouter from "./routes/cours.route.js";
 import classeRouter from "./routes/classe.route.js";
-import foyerRoutes from './routes/foyerRoutes.js';
-import menuRoutes from './routes/menuRoutes.js';
-import reservationRoutes from './routes/reservationRoutes.js';
-import commentRoutes from './routes/commentRoutes.js'; 
-import restaurantRoutes from './routes/restaurantRoutes.js';  
-import roomRoutes from './routes/roomRoutes.js';
-import paymentRoutes from './routes/payment.route.js';
+import foyerRoutes from "./routes/foyerRoutes.js";
+import menuRoutes from "./routes/menuRoutes.js";
+import reservationRoutes from "./routes/reservationRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import restaurantRoutes from "./routes/restaurantRoutes.js";
+import roomRoutes from "./routes/roomRoutes.js";
+import paymentRoutes from "./routes/payment.route.js";
+import eventRoute from "./routes/event.route.js";
+import userRoute from "./routes/user.route.js";
+import { init as initSocketService } from "./Socket/socketService.js";
+import http from "http";
 import { errorHandler, notFoundError } from "./middlewares/error-handler.js";
-
-
 
 const app = express();
 const databaseName = "uninet";
 
-mongoose.connect(`mongodb://localhost:27017/${databaseName}`, { family: 4 })
-    .then(() => {
-        console.log("database connected");
-    })
-    .catch((e) => {
-        console.log(e);
-    });
-
+mongoose
+  .connect(`mongodb://localhost:27017/${databaseName}`, { family: 4 })
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+const server = http.createServer(app);
+const io = initSocketService(server);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,28 +42,29 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 9090;
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 
-app.use('/api/foyers', foyerRoutes);
-app.use('/api/menus', menuRoutes);
-app.use('/api/reservations', reservationRoutes); 
-app.use('/api/comments', commentRoutes);  
-app.use('/api/restaurants', restaurantRoutes);  
-app.use('/api/rooms', roomRoutes);
- 
+app.use("/api/foyers", foyerRoutes);
+app.use("/api/menus", menuRoutes);
+app.use("/api/reservations", reservationRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/restaurants", restaurantRoutes);
+app.use("/api/rooms", roomRoutes);
 
-app.use('/api/auth', authRoutes)
-app.use("/user", userRouter)
-app.use("/university", universityRouter)
-app.use('/api/payment', paymentRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/user", userRouter);
+app.use("/university", universityRouter);
+app.use("/api/payment", paymentRoutes);
 
-app.use("/niveauEtude", niveauRouter );
-app.use("/cours", coursRouter );
-app.use("/classe", classeRouter );
+app.use("/niveauEtude", niveauRouter);
+app.use("/cours", coursRouter);
+app.use("/classe", classeRouter);
 
+app.use("/api/events", eventRoute(io));
+app.use("/api/user", userRoute);
 
-app.use(notFoundError)
-app.use(errorHandler)
+app.use(notFoundError);
+app.use(errorHandler);
 app.listen(PORT, hostname, () => {
-    console.log(`Server running on http://${hostname}:${PORT}`);
-  });
+  console.log(`Server running on http://${hostname}:${PORT}`);
+});
