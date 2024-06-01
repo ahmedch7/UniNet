19541f9d574e55c906809c39c7ef608fec89dfbc
AzizLachkar1 <mohamedaziz.lachkar@esprit.tn>
2024-06-01 09:57:07 +0200

    Add files via upload

---
+++ routes/event.route.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import {
+    getEvents,
+    getEvent,
+    createEvent,
+    updateEvent,
+    deleteEvent,
+    likeEvent,
+    dislikeEvent,
+    addComment,
+    deleteComment,
+    updateComment
+} from '../controllers/event.controller.js';
+
+export default (io) => {
+    const router = express.Router();
+
+    router.get('/', getEvents);
+    router.get('/:id', getEvent);
+    router.post('/', createEvent);
+    router.put('/:id', updateEvent);
+    router.delete('/:id', deleteEvent);
+    router.post('/:id/like', likeEvent);
+    router.post('/:id/dislike', dislikeEvent);
+    router.post('/:id/comments', (req, res) => addComment(io, req, res));
+    router.put('/:eventId/comments/:commentId', updateComment);
+    router.delete('/:eventId/comments/:commentId', deleteComment);
+
+    return router;
+};
