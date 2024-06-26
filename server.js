82a127c147b55908ccfcb1c207d47e0d1cbe6167
Intissar NAJJAR <106880579+intissarNJ@users.noreply.github.com>
2024-06-27 00:44:26 +0100

    fix

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import cors from "cors";
 import mongoose from "mongoose";
 import errorHandlers from "./middlewares/error-handler.js";
 const { errorHandler, notFoundError } = errorHandlers; // Destructure the required exports
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 import commentaireRoutes from "./routes/commentaire.route.js";
 import forumRoutes from "./routes/forum.route.js";
 import postRoutes from "./routes/post.route.js";
+import http from "http";
 
 const app = express();
 const databaseName = "uninet";
@@ -21,7 +22,14 @@ mongoose
     console.log(e);
   });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
@@ -36,6 +44,23 @@ app.use("/forum", forumRoutes);
 
 app.use(notFoundError);
 app.use(errorHandler);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:4200/", // Replace with your Angular app URL
+    methods: ["GET", "POST"],
+  },
+});
+
+// Handle socket connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Example event
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 app.listen(PORT, hostname, () => {
   console.log(`server running on http://${hostname}:${PORT}`);
