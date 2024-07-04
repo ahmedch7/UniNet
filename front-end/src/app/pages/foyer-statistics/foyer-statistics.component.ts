c7a0594561fbf003a01a5c6c31c4bf8b3072cb97
Talel Kbaier <tkbaier@triweb.local>
2024-07-04 12:05:04 +0100

    telechargerExcel et pagination

--- front-end/src/app/pages/foyer-statistics/foyer-statistics.component.ts
+++ front-end/src/app/pages/foyer-statistics/foyer-statistics.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FoyerService } from 'src/app/Services/foyer.service';
 import { RoomService } from 'src/app/Services/room.service';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-foyer-statistics',
@@ -9,13 +11,15 @@ import { RoomService } from 'src/app/Services/room.service';
 })
 export class FoyerStatisticsComponent implements OnInit {
   foyers: any[] = [];
+  displayedFoyers: any[] = [];
   roomsByFoyer: { [key: string]: any[] } = {}; // Stocke les chambres pour chaque foyer
   totalFoyers: number = 0;
   totalRooms: number = 0;
   totalAvailablePlaces: number = 0;
+  itemsPerPage: number = 5;
+  currentPage: number = 1;
 
- 
-  constructor(private foyerService: FoyerService, private roomService: RoomService, private renderer: Renderer2) { }
+  constructor(private foyerService: FoyerService, private roomService: RoomService) { }
 
   ngOnInit(): void {
     this.loadFoyers();
@@ -27,6 +31,7 @@ export class FoyerStatisticsComponent implements OnInit {
         this.foyers = data;
         this.totalFoyers = this.foyers.length;
         this.loadRoomsForAllFoyers();
+        this.updateDisplayedFoyers();
       },
       (error) => {
         console.error('Error loading foyers', error);
@@ -62,4 +67,42 @@ export class FoyerStatisticsComponent implements OnInit {
   getAvailablePlaces(foyerId: string): number {
     return this.roomsByFoyer[foyerId] ? this.roomsByFoyer[foyerId].reduce((acc, room) => acc + room.availablePlaces, 0) : 0;
   }
+
+  onPageChanged(event: Event, page: number): void {
+    event.preventDefault();
+    if (page >= 1 && page <= this.getTotalPages()) {
+      this.currentPage = page;
+      this.updateDisplayedFoyers();
+    }
+  }
+
+  updateDisplayedFoyers(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.displayedFoyers = this.foyers.slice(startIndex, endIndex);
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.totalFoyers / this.itemsPerPage);
+  }
+
+  getPaginationArray(): number[] {
+    const totalPages = this.getTotalPages();
+    return Array(totalPages).fill(0).map((_, i) => i + 1);
+  }
+
+  downloadExcel(): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.foyers);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    this.saveAsExcelFile(excelBuffer, 'foyers');
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
+    FileSaver.saveAs(data, `${fileName}_export_${new Date().getTime()}.xlsx`);
+  }
 }
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
